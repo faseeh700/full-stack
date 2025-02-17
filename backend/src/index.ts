fdb2c7b16ec042d9bc1d8ffc0a1a9eb859a0ca44
faseeh700 <fasoooo49@gmail.com>
2025-02-17 11:30:59 +0000

    today work finised

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,8 +14,9 @@ app.use(express.json())
 
 
 app.get("/test-db",async(req,res)=>{
+
     try {
-        const result = await pool.query('SELECT * FROM students')
+        const result = await pool.query(`SELECT * FROM students`)
         res.json(result)
     } catch (error) {
         res.status(500).json({error:(error as Error).message})
@@ -29,7 +30,7 @@ app.post("/users",async(req,res)=>{
         const { first_name, last_name, date_of_birth, email, enrollment_date, major, gpa } = req.body;
         
         const insertUsers = await pool.query(`INSERT INTO students(first_name, last_name, date_of_birth, email, enrollment_date, major, gpa) 
-            VALUES($1, $2, $3, $4, $5, $6, $7)`,
+            VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id, first_name, last_name`, //returning use for what we insert it will return in the result
             [first_name, last_name, date_of_birth, email, enrollment_date, major, gpa]
             );
             res.status(201).json(insertUsers)
