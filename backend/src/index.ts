831600599b93bb8add6d458dc127996fdae403ac
faseeh700 <fasoooo49@gmail.com>
2025-02-17 14:25:16 +0000

    update route id doing problem

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ app.get("/test-db",async(req,res)=>{
 
     try {
         const result = await pool.query(`SELECT * FROM students`)
-        res.json(result)
+        res.json(result.rows)
     } catch (error) {
         res.status(500).json({error:(error as Error).message})
     }
@@ -30,15 +30,36 @@ app.post("/users",async(req,res)=>{
         const { first_name, last_name, date_of_birth, email, enrollment_date, major, gpa } = req.body;
         
         const insertUsers = await pool.query(`INSERT INTO students(first_name, last_name, date_of_birth, email, enrollment_date, major, gpa) 
-            VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id, first_name, last_name`, //returning use for what we insert it will return in the result
+            VALUES($1, $2, $3, $4, $5, $6, $7)`, //returning use for what we insert it will return in the result
             [first_name, last_name, date_of_birth, email, enrollment_date, major, gpa]
             );
-            res.status(201).json(insertUsers)
+            res.status(201).json(insertUsers.rows)
     } catch (error) {
         console.error(error); // Log error to troubleshoot
         res.status(500).json({ error: error });
     }
 })
+
+
+app.put("/users/:id",async(req,res)=>{
+    const{id} = req.params
+    const{major} = req.body
+    try {
+        const updateData = await pool.query(`UPDATED students SET major = $1 WHERE id = $2 RETURNING`,[major,id]
+
+            
+        )
+        if(updateData.rowCount === 0){
+            return res.status(400).json("user dont exist")
+        }
+        res.status(200).json(updateData.rowCount)
+
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
   
 
 
