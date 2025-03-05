cd3f860ef9890c7afed7f240350684c3c3c8c2e1
faseeh700 <fasoooo49@gmail.com>
2025-03-05 12:15:29 +0500

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,23 +3,22 @@ import express from "express";
 import studentRoutes from "./routes/studentRoutes";
 import { initDB } from "./db/pool";
 import authRoutes from "./routes/authRoutes";
-import userRoutes from "./routes/userRoutes"
+import userRoutes from "./routes/userRoutes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());
-
+app.use(cookieParser());
 // create new Tables from here
 // initDB()
-
 
 // Routes
 app.use("/api", studentRoutes);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-
 
 // Start server
 app.listen(PORT, () => {
